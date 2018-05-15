bd70979c97fa96a830c5992f05e837f0cf36944e
Carl Filler <carlfiller@gmail.com>
2018-05-14 19:36:28 -0500

    First draft of printJobs loop

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -110,9 +111,14 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        HashMap<String, String> key = someJobs.get(0);
-        String keyOne = key.get
-        if (someJobs)
+        for (int i = 0; i < someJobs.size(); i++) {
+            HashMap<String, String> job = new HashMap<>(someJobs.get(i));
+            System.out.println("*****");
+            for (Map.Entry<String, String> entry : job.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+            System.out.println("*****");
+        }
         System.out.println("printJobs is not implemented yet");
         // if ()
     }
